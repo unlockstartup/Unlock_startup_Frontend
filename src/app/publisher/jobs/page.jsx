6f3d6361a5d3ed8317a158bfc618711b0c3e29a4
Manav Jain <manav7421@gmail.com>
2026-06-11 10:58:22 +0530

    fixes

--- src/app/publisher/jobs/page.jsx
+++ src/app/publisher/jobs/page.jsx
@@ -284,8 +284,15 @@ const validate = () => {
 const stripHtml = (html) => {
   if (!html) return "";
   return html
-    .replace(/\s*style="[^"]*"/gi, "")
-    .replace(/\s*style='[^']*'/gi, "");
+    .replace(/<br\s*\/?>/gi, "\n")
+    .replace(/<\/p>/gi, "\n")
+    .replace(/<[^>]+>/g, "")
+    .replace(/&nbsp;/gi, " ")
+    .replace(/&amp;/gi, "&")
+    .replace(/&lt;/gi, "<")
+    .replace(/&gt;/gi, ">")
+    .replace(/\n{3,}/g, "\n\n")
+    .trim();
 };
 
 const saveJob = async () => {
