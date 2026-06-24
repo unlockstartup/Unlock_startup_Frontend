14d62156370a26db2894967a47c266b26c07cc48
Manav Jain <manav7421@gmail.com>
2026-06-24 12:54:22 +0530

    refresh

--- src/app/apiServices/serviceplan.js
+++ src/app/apiServices/serviceplan.js
@@ -1,11 +1,11 @@
-import api from "@/app/api";
+import publisherApi from "../publisherapi";
 
-export const getServicePlans = () => api.get("/api/service-plans");
+export const getServicePlans = () => publisherApi.get("/api/service-plans");
 
 export const createServiceOrder = (servicePlanId, durationType) =>
-  api.post("/api/service-plans/create-order", { servicePlanId, durationType });
+  publisherApi.post("/api/service-plans/create-order", { servicePlanId, durationType });
 
 export const verifyServicePayment = (payload) =>
-  api.post("/api/service-plans/verify", payload);
+  publisherApi.post("/api/service-plans/verify", payload);
 
 export default { getServicePlans, createServiceOrder, verifyServicePayment };
