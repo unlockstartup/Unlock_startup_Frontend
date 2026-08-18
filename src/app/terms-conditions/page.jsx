0ca6edc6b96b76a571ca0020445ed03a7f639fbb
Manav Jain <manav7421@gmail.com>
2026-08-18 12:41:24 +0530

    changes

--- src/app/terms-conditions/page.jsx
+++ src/app/terms-conditions/page.jsx
@@ -115,7 +115,7 @@ export default function TermsConditions() {
               <div>
                 <h2 className="ex-section-title">Introduction</h2>
                 <p className="ex-section-subtitle">Welcome to Unlock Startup Platform</p>
-                 <p>Terms & Conditions wording for a brand called “Unlock Startup”, with the company name “World Mirror Innovations Pvt Ltd”.</p>
+                 <p className="ex-section-subtitle">Terms & Conditions wording for a brand called “Unlock Startup”, with the company name “World Mirror Innovations Pvt Ltd”.</p>
               </div>
             </div>
             <div className="ex-prose">
