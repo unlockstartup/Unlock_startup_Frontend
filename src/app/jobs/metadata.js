64f4f5f228586d356722e327d5dc74a24e205231
unlockstartup <unlockstartupinfo@gmail.com>
2026-09-21 22:46:17 +0530

    Update metadata.js

--- src/app/jobs/metadata.js
+++ src/app/jobs/metadata.js
@@ -3,7 +3,7 @@ export const metadata = {
   description:
     "Find startup jobs and internships in India across technology, IT, digital marketing, business development, sales, operations and more. Search jobs by location, work mode, job type and experience level. Apply for startup jobs online. Call +91-9266733959.",
   keywords:
-    "expert panel discussions,startup events in India,business networking events in India,entrepreneur workshops in India,startup pitch competitions in,India,online startup webinars,upcoming startup conferences,startup expo events, innovation forums for startups,investor networking events founder meetups in India,professional seminars for entrepreneurs,startup panel discussions.",
+    "expert panel discussions,startup events in India,business networking events in India,entrepreneur workshops in India,startup pitch competitions in,India,online startup webinars,upcoming startup conferences,startup expo events, innovation forums for startups,investor networking eventsfounder meetups in India,professional seminars for entrepreneurs,startup panel discussions.",
   robots: "index, follow",
   alternates: {
     canonical: "https://www.unlockstartup.com/jobs",
