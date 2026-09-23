7f0771aab4901f73dead1bc98373e6c3699b7237
unlockstartup <unlockstartupinfo@gmail.com>
2026-09-23 21:28:14 +0530

    Update page.jsx

--- src/app/investors/page.jsx
+++ src/app/investors/page.jsx
@@ -168,15 +168,3 @@ const filtered = useMemo(() => {
     </main>
   );
 }
-
-export const metadata = {
-  title: "Startup Investors in India | Angel Investors & VCs | Unlock Startup",
-  description:
-    "Find startup investors in India, including angel investors, venture capital firms and funding partners. Discover investors by investment type, startup stage and location, and connect with potential investors for your business. Call +91-9266733959.",
-  keywords:
-    "startup internship in Delhi, digital marketing internship Delhi, part time startup jobs Delhi, freelance startup jobs India contract jobs in startups, work from office startup jobs, full time digital marketing jobs Delhi, startup jobs for freshers Delhi,paid internship in startups, Delhi startup hiring jobs",
-  robots: "index, follow",
-  alternates: {
-    canonical: "https://www.unlockstartup.com/investors",
-  },
-};
