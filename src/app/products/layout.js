c81d625949f8f77265f83c8edce64c4c3448e3ae
unlockstartup <unlockstartupinfo@gmail.com>
2026-09-23 22:32:48 +0530

    Add files via upload

---
+++ src/app/products/layout.js
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "Startup Products in India | Discover Innovative Products | Unlock Startup",
+  description:
+    "Discover innovative startup products in India across different categories and product stages. Explore products by category, product status and patent status on Unlock Startup. Call +91-9266733959 for more information.",
+  robots: "index, follow",
+  alternates: {
+    canonical: "https://www.unlockstartup.com/products",
+  },
+};
+
+export default function Layout({ children }) {
+  return children;
+}
